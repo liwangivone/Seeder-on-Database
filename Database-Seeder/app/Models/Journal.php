8b9b149cefc86e4c027245c1c5d44cdbeff8247c
liwangivone <liwangivone@gmail.com>
2024-11-17 22:39:49 +0800

    model and migration

---
+++ Database-Seeder/app/Models/Journal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Journal extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'title',
+        'author',
+        'published_year',
+        'publisher',
+        'stock'
+    ];
+}
